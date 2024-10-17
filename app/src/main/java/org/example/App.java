8a45b3250f064885b257f4c27d2f14b803be49f3
GabryelMelo <gabryel.melo@aluno.riogrande.ifrs.edu.br>
2024-10-17 20:35:05 -0300

    add jacoco & spotless plugins

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -2,12 +2,11 @@ package org.example;
 
 public class App {
 
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Olá mundo!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
-    
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+  }
 }
